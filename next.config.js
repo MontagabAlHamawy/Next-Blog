ceb38e5469dea829943276403bdc3333caec5964
Montagab Al-Hamawy <ma0152260@gmail.com>
2023-09-26 15:50:13 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,25 +1,9 @@
-const path = require('path');
-
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
-  // إعدادات أخرى إذا كنت بحاجة إليها
-  // ...
-
-  // إعدادات الإخراج (output) للتصدير الثابت
-  output: {
-    // تحديد نوع الإخراج كـ "export"
-    // بدلاً من استخدام كائن
-    // يجب أن يكون القيمة هنا "export"
-    // إذا كنت تستخدم "next export"
-    type: 'export',
-
-    // مسار المجلد الذي سيتم تصدير الملفات إليه
-    // يمكنك تغييره حسب احتياجاتك
-    path: path.join(__dirname, 'out'),
-
-    // ضبط أي إعدادات إضافية هنا
-    // ...
-  },
-};
-
-module.exports = nextConfig;
+  output: 'export',
+ 
+}
+ 
+module.exports = nextConfig
