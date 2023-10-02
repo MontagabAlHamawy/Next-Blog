7183d3fb5fe96bc89cb5cb132f0c884aea722e84
Montagab Al-Hamawy <ma0152260@gmail.com>
2023-10-02 20:25:46 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,19 +1,25 @@
-/**
- * @type {import('next').NextConfig}
- */
-const nextConfig = {
-  output: 'export',
-  basePath: 'https://montagabalhamawy.github.io/Next-Blog',
-  assetPrefix: 'https://montagabalhamawy.github.io/Next-Blog/',
-   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: 'montagabalhamawy.github.io/Next-Blog'
-      }
+// /**
+//  * @type {import('next').NextConfig}
+//  */
+// const nextConfig = {
+//   output: 'export',
+//   basePath: 'https://montagabalhamawy.github.io/Next-Blog',
+//   assetPrefix: 'https://montagabalhamawy.github.io/Next-Blog/',
+//    images: {
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: 'montagabalhamawy.github.io/Next-Blog'
+//       }
 
-    ]
-  }
-}
+//     ]
+//   }
+// }
  
-module.exports = nextConfig
+// module.exports = nextConfig
+
+module.exports = {
+  basePath: '/Next-Blog', // اسم مجلد مشروعك على GitHub Pages
+  assetPrefix: '/Next-Blog/', // نفس اسم المجلد
+}
+
