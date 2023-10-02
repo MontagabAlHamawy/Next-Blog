edba1186b28b72303088a0e826140391d12b47f6
Montagab Al-Hamawy <ma0152260@gmail.com>
2023-10-02 20:55:15 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   assetPrefix: '/Next-Blog', // مسار قاعدة المشروع على GitHub Pages
   basePath: '/Next-Blog', // أيضا قم بتحديد basePath بنفس القيمة
 }
