1d15c10662ef9cdc8ac3b2effc365b6c50a064be
Montagab Al-Hamawy <ma0152260@gmail.com>
2023-10-02 19:54:29 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,9 +2,9 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-  // output: 'export',
-  // basePath: '/Next-Blog',
-  // assetPrefix: '/Next-Blog/',
+  output: 'export',
+  basePath: '/Next-Blog',
+  assetPrefix: '/Next-Blog/',
    images: {
     remotePatterns: [
       {
