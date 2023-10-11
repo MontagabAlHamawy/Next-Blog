6a3ebef544b2662909c1409e437abc444b961116
Montagab Al-Hamawy <ma0152260@gmail.com>
2023-10-11 21:21:34 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,7 +4,16 @@ const withImages = require('next-images');
 const nextConfig = {
   output: 'export',
   assetPrefix: '/Next-Blog', // مسار قاعدة المشروع على GitHub Pages
-  basePath: '/Next-Blog', // أيضا قم بتحديد basePath بنفس القيمة
+  basePath: '/Next-Blog', 
+   images: {
+    domains: ['montagabalhamawy.github.io/Next-Blog/'], // قائمة بالنطاقات المسموح بها لعرض الصور
+  },
 }
 
 module.exports = withImages(nextConfig);
+// next.config.js
+module.exports = {
+  images: {
+    domains: ['example.com'], // قائمة بالنطاقات المسموح بها لعرض الصور
+  },
+};
