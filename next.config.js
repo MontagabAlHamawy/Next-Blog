4c89827b04f24169b148a2469939477db0df69a9
MontagabAlHamawy <ma0152260@gmail.com>
2024-02-20 20:35:08 +0300

    vercel apdate

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // distDir: "build",
-  // productionBrowserSourceMaps: true,
-  output: "export"
+  output: "export",
 };
+
 module.exports = nextConfig;
